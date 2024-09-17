f27547a16a44cc6ed5befb1b407327279b056ee4
Sumihiri <sumihiri.nisansala@gmx.com>
2024-09-17 15:04:01 +0200

    first commit

---
+++ src/main/java/se/sumihiri/aplicationdb/AplicationdbApplication.java
@@ -0,0 +1,13 @@
+package se.sumihiri.aplicationdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/sumihiri/aplicationdb/AplicationdbApplicationTests.java
@@ -0,0 +1,11 @@
+package se.sumihiri.aplicationdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationdbApplicationTests {
+
+
+
+}
